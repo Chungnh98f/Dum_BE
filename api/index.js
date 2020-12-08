833e158d46d42719444206e562b55c9d673a3adc
chungnh98f <chungnguyen011298@gmail.com>
2020-12-08 10:47:16 +0700

    add get api test to mainpage

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const { route } = require("./auth");
 
 const authRouter = require("./auth");
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+
+router.get("/", (req, res) => {
+  res.json("Hello world");
+});
 
 module.exports = router;
